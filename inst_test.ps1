2ff785ce7eeff855ebc10602af0ace9f4818af20
Lord Mike <lordmike@iki.fi>
2024-11-02 23:12:26 +0200

    simple powershell script test

---
+++ inst_test.ps1
@@ -0,0 +1,26 @@
+#C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe
+
+# Copyright (c) 2024, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+# 
+# License of this script file:
+#   MIT License
+# 
+# License is available online:
+#   https://github.com/lordmikefin/auto_inst_test_win11/blob/main/LICENSE
+# 
+# Latest version of this script file:
+#   https://github.com/lordmikefin/auto_inst_test_win11/blob/master/inst_test.ps1
+
+# inst_test.ps1
+# Testing powershell script instead of batch script.
+
+$Current_script_ver="0.0.1"
+$Current_script_date="2024-11-02"
+$Current_script="inst_test.ps1"
+
+Write-Host "Current_script_ver: ${Current_script_ver} (${Current_script_date})"
+
+
+
+Write-Host ""
+Write-Host "End of script '${Current_script}'"
